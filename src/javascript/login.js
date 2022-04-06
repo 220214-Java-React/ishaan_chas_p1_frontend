1ed471295523cb967ddbe159acd9769480887f45
Chasls <chasstevens1219@gmail.com>
2022-04-06 00:58:01 -0500

    updated the login
    login features fully functioning

--- src/javascript/login.js
+++ src/javascript/login.js
@@ -13,47 +13,53 @@ async function loginUser() {
     if (login.username && login.password) {
         const currentUserJSON = JSON.stringify(login);
 
-        let data = await fetch("http://localhost:8080/login", {
+        let yup = await fetch("http://localhost:8080/login/*", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: currentUserJSON
         })
-            .then(res => res.json());
-        console.log(data);
+            .then(res => res.json())
+            .then(data => getData(data))
 
 
-        let { user_id, role_id1 } = data;
-        console.log(data);
-
-        localStorage.setItem('role_id1', role_id1);
-        localStroage.setItem('user_id', user_id);
-
-        switch (role_id1) {
-
-            case 'a':
-                window.location.href = '';
-                break;
-
-            case 'f':
-                window.location.href = ''
-                break;
-
-            case 'e':
-                window.location.href = 'C:\Users\Chasls\Documents\GitHub\ishaan_chas_p1_frontend\src\Index.html'
-                break;
-
-            default:
-                window.location.href = ''
-
-        }
+    
     }
 
-    if (Response.status == 200) {
+    if (resp.status == 200) {
         alert("You have logged in");
     } else {
         alert("Login Failed")
     }
 
 }
+
+function getData(data){
+
+    let {user_id, role_id1 } = data;
+        console.log(data);
+
+        localStorage.setItem('role_id1', role_id1);
+        localStorage.setItem('user_id', user_id);
+
+        switch (role_id1) {
+
+            case 'a':
+                window.location.href = 'Index.html';
+                break;
+
+            case 'f':
+                window.location.href = 'Index.html'
+                break;
+
+            case 'e':
+                window.location.href = 'Index.html'
+                break;
+
+            default:
+                window.location.href = 'Index.html'
+
+        }
+
+    }
