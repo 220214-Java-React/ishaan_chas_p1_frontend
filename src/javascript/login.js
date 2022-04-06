e8c999bd590e224240cd744ec482f664fb1c8bd8
Chasls <chasstevens1219@gmail.com>
2022-04-05 21:20:54 -0500

    adding more login fucntionality
    fixing some bugs for the login

--- src/javascript/login.js
+++ src/javascript/login.js
@@ -1,11 +1,8 @@
- 
- let loginSubmit = document.getElementById(login);
- if(loginSubmit){
-     loginSubmit.addEventListener('click', loginUser())
- }
- 
- function loginUser(){
-   
+
+
+
+async function loginUser() {
+
 
 
     let login = {
@@ -13,34 +10,50 @@
         password: document.getElementById("password").value,
     }
 
-        if(login.username && login.password){
-            const currentUserJSON = JSON.stringify(login);
+    if (login.username && login.password) {
+        const currentUserJSON = JSON.stringify(login);
 
-            let data = await fetch("http://localhost:8080/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: currentUserJSON
-            })
+        let data = await fetch("http://localhost:8080/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: currentUserJSON
+        })
             .then(res => res.json());
-            console.log(data);
-        }
+        console.log(data);
 
-        let {user_id, role_id1} = data;
+
+        let { user_id, role_id1 } = data;
         console.log(data);
 
         localStorage.setItem('role_id1', role_id1);
         localStroage.setItem('user_id', user_id);
 
-        if(role_id1 == manager){
-                    document.getElementById();
-        }
+        switch (role_id1) {
 
-        if(response.status == 200){
-            alert("Successful Account Creation");
-          }else{
-            alert("Account creation unsuccessful.")
-          }
+            case 'a':
+                window.location.href = '';
+                break;
+
+            case 'f':
+                window.location.href = ''
+                break;
+
+            case 'e':
+                window.location.href = 'C:\Users\Chasls\Documents\GitHub\ishaan_chas_p1_frontend\src\Index.html'
+                break;
+
+            default:
+                window.location.href = ''
+
+        }
+    }
+
+    if (Response.status == 200) {
+        alert("You have logged in");
+    } else {
+        alert("Login Failed")
+    }
 
 }
